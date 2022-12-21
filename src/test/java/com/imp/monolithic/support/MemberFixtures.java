8af7ed086107bf7d3ab0e6cd7b26c528022780fe
Richard JEON <ztzy1907@gmail.com>
2022-12-21 17:47:03 +0900

    feat: 테스트 격리 빈 생성
    Co-authored-by: Richard Jeon <ztzy1907@gmail.com>
    Co-authored-by: Hyunta <mohai2618@gmail.com>
    Co-authored-by: kimchan123 <gmelddl514@gmail.com>
    Co-authored-by: Hongdonggeon <ghd700@naver.com>

---
+++ src/test/java/com/imp/monolithic/support/MemberFixtures.java
@@ -0,0 +1,28 @@
+package com.imp.monolithic.support;
+
+import com.imp.monolithic.member.Member;
+import com.imp.monolithic.member.Role;
+
+public enum MemberFixtures {
+
+    KUN("ghd700@email.com", Role.ADMIN, "password", "kun"),
+    ;
+
+    private final String email;
+    private final Role role;
+
+    private final String password;
+
+    private final String nickname;
+
+    MemberFixtures(final String email, final Role role, final String password, final String nickname) {
+        this.email = email;
+        this.role = role;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    public Member create() {
+        return new Member(role, email, nickname, password);
+    }
+}
